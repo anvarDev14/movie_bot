6df757c092dec1f7b1bf0aa9b8d92db40ca09f5e
anvarDev <amujinov@gmail.com>
2024-11-23 15:51:06 +0500

    upgrateAll files

--- data/config.py
+++ data/config.py
@@ -6,5 +6,5 @@ env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = list(map(int, env.list("ADMINS")))
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
