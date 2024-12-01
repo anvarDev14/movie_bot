c2c8e0c67187567b35354739f0e4a50ea48e02f3
anvarDev <amujinov@gmail.com>
2024-12-01 12:59:13 +0500

    abs

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -7,8 +7,8 @@ async def set_default_commands(dp):
             types.BotCommand("start", "Botni ishga tushurish, Kino qidirish"),
             types.BotCommand("help", "Yordam"),
             types.BotCommand("admin","Admin uchun menyu"),
-            # types.BotCommand("add_movie","Kino qo'shish"),
-            # types.BotCommand("delete_movie","Kinoni o'chirish"),
-            # types.BotCommand("update","captionni yangilash")
+            types.BotCommand("add_movie","Kino qo'shish"),
+            types.BotCommand("delete_movie","Kinoni o'chirish"),
+            types.BotCommand("update","captionni yangilash")
         ]
     )
